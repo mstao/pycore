a265134e294ac635a8c8dba5b9b558744f42925c
Juntao Han <499445428@qq.com>
2019-06-30 00:37:19 +0800

    Initial commit

---
+++ src/base/if.py
@@ -0,0 +1,17 @@
+"""
+if <条件判断1>:
+    <执行1>
+elif <条件判断2>:
+    <执行2>
+elif <条件判断3>:
+    <执行3>
+else:
+    <执行4>
+"""
+age = 3
+if age >= 18:
+    print('adult')
+elif age >= 6:
+    print('teenager')
+else:
+    print('kid')
